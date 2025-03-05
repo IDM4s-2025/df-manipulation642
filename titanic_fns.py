3899b4d6e0b91ca2b2ae834c410a7f2e35e5fa92
Polito9 <colin.reyes.diego@gmail.com>
2025-03-05 17:38:15 -0600

    Separating modules

---
+++ titanic_fns.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def getPassengerName_byId(df:pd.DataFrame,id: int) -> str:
+    """Returns the name of the passenger according to the specified id
+
+    Args:
+        df (pd.DataFrame): Dataframe used
+        id (int): Id of passenger
+
+    Returns:
+        str: A string conaining the name of the passenger
+    """
+    return df[df.PassengerId == id].Name.iloc[0]
+
+def getPassengerId_byName(df:pd.DataFrame, name: str) -> int:
+    """Returns the id of the passenger according to a given name
+
+    Args:
+        df (pd.DataFrame): DataFrame used
+        name (str): Name of the passenger
+
+    Returns:
+        int: Id of the passenger
+    """
+    return int(df[df.Name == name]["PassengerId"].iloc[0])
