8c295adf8361013543f6a35a3a87edaaf7cff45d
Mario Carlos Gaitan Reyna <A01659057@tec.mx>
2025-03-12 15:11:04 -0600

    Ejercicio completo

--- titanic_fns.py
+++ titanic_fns.py
@@ -34,10 +34,23 @@ def pregunta_10(df_train):
     df_Ma_S.head()
     len(df_Ma_S)
     len(df_Ma_NS)
-    print(f"La suma total de mujeres y niños eran {len(df_Ma_S)+len(df_Ma_NS)}, de los cuales sobrevivieron {len(df_Ma_S)} y no sobrevivieron {len(df_Ma_NS)}")
+    print(f"La suma total de hombres era {len(df_Ma_S)+len(df_Ma_NS)}, de los cuales sobrevivieron {len(df_Ma_S)} y no sobrevivieron {len(df_Ma_NS)}")
     print(f"El porcentaje sería {round(len(df_Ma_S)/len(df_Ma_S+df_Ma_NS)*100,2)}%")
     print("Por lo que se puede intuir que las mujeres y niños tuvieron una mayor tasa de supervivencia que los hombres mayores de 15 años")
 def pregunta_11(df_train):
     print(df_train.Age.mean())
 def pregunta_12(df_train):
     print(df_train.Embarked.value_counts())
+def pregunta_13(df_train):
+    surv_14 = df_train.groupby("Survived")["Pclass"].value_counts()
+    print(surv_14)
+    #print(surv_14.loc[1,3])
+    porcentaje3_s = (surv_14.loc[1,3]/(surv_14.loc[1,3]+surv_14.loc[0,3]))*100
+    porcentaje2_s = (surv_14.loc[1,2]/(surv_14.loc[1,2]+surv_14.loc[0,2]))*100
+    porcentaje1_s = (surv_14.loc[1,1]/(surv_14.loc[1,1]+surv_14.loc[0,1]))*100
+    print(f"El porcentaje de la clase 3 de sobrevivientes es: {round(porcentaje3_s,2)}%")
+    print(f"El porcentaje de la clase 2 de sobrevivientes es: {round(porcentaje2_s,2)}%")
+    print(f"El porcentaje de la clase 1 de sobrevivientes es: {round(porcentaje1_s,2)}%")
+def pregunta_boolean(df_train ,boolean):
+    surv = df_train[boolean][(df_train.Survived == 1)&(boolean == True)]
+    print(f"El porcentaje de sobrevivientes es {round(len(surv)/len(df_train)*100,2)}%")
