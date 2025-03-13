9b4ec42db629a5d2d9e886bc945f15389bb4a91d
AxelPalacios3 <A01666972@tec.mx>
2025-03-13 12:42:28 -0600

    Cuaderno

--- titanic.py
+++ titanic.py
@@ -1,33 +1,19 @@
 import pandas as pd
-tabla=pd.read_csv ('dataaa/train.csv')
 
-tabla
-tabla[tabla.PassengerId==1].Name
+tabla = pd.read_csv('dataaa/train.csv')
+def id_to_name(df, id):
+    return df.loc[df["PassengerId"] == id, "Name"].iloc[0]
 
-def id_to_name(df: pd.DataFrame, id:int)->str:
-    """_summary_
+def nameid(df, name):
+    return df.loc[df["Name"] == name, "PassengerId"].iloc[0]
 
-    Args:
-        df (pd.DataFrame): _description_
-        id (int): _description_
-
-    Returns:
-        str: _description_
-    """
-    result = df.loc[df["PassengerId"] == id, "Name"]
-    return result.iloc[0] 
-def name_to_id(df: pd.DataFrame, name: str) -> int:
-    result = df.loc[df["Name"] == name, "PassengerId"]
-    return result.iloc[0] 
-
-    return "nombre de pasajero"
-id_to_name(tabla,1)
 nombre = "Montvila, Rev. Juozas"
-passengerid = name_to_id(tabla, nombre)
+passengerid = nameid(tabla, nombre)
 pnombre = id_to_name(tabla, 42)
-oldpassenger = tabla.loc[tabla["Age"] == tabla["Age"].max()]
+old = tabla.loc[tabla["Age"] == tabla["Age"].max()]
 
-print(f"El ID del pasajero  {nombre} es {passengerid}")
-print(f"El pasajero con ID 42 es {pnombre}")
-print('El pasajero con más edad es\n')
-print(oldpassenger)
+print(f"El ID del pasajero {nombre} es {passengerid}")
+print(f"42 = {pnombre}\n")
+print("El pasajero con más edad es:\n",old)
+
+
